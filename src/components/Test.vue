c492833913ae2e93cb697717196b49931af135b7
narcis13 <narcis75@gmail.com>
2023-09-24 22:20:56 +0300

    cu vue

---
+++ src/components/Test.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const el = ref('test')
+
+onMounted(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => el.value=json.title)
+})
+</script>
+
+<template>
+    <h1>Component VUE {{ el }}</h1>
+</template>
